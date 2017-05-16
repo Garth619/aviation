fcf241ff43fabb86daa1f8d8ae274e25096b4221
Garrett Cullen <garrettcullen@Garretts-iMac.local>
2017-05-16 13:25:57 -0700

    Push

--- wp-content/themes/psb/page.php
+++ wp-content/themes/psb/page.php
@@ -11,12 +11,37 @@ get_header(); ?>
 
 			<?php include('banner.php');?>
 			
+			<div class="inner_main">
+				
+				
+				<div class="container_wrapper">
+				
+					<div class="container">
+					
+						<h1 class="title_header">nationwide aviation disaster attorneys</h1><!-- title_header -->
+						
+						<div class="purple_line_wrapper">
+							
+							<div class="purple_line"></div><!-- purple_line -->
+							
+							<div class="purple_square"></div><!-- purple_square -->
+							
+						</div><!-- purple_line_wrapper -->
+						
+						<div class="content"></div><!-- content -->
+					
+						<?php // get_template_part( 'loop', 'page' ); ?>
+					
+					</div><!-- container -->
+				
+				
+				
+				<?php get_sidebar();?>
+				
+				</div><!-- content_wrapper -->
+				
+				
+			</div><!-- inner_main -->
 			
 			
-			
-			
-			
-			<?php // get_template_part( 'loop', 'page' ); ?>
-
-
-<?php get_footer(); ?>
+			<?php get_footer(); ?>
