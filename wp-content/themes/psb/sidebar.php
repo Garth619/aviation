f3e6238b1d40197870eadd6a51946bc7e3f0c93c
Garrett Cullen <garrettcullen@Garretts-iMac.local>
2017-05-16 15:30:34 -0700

    Push

--- wp-content/themes/psb/sidebar.php
+++ wp-content/themes/psb/sidebar.php
@@ -1,1 +1,26 @@
-<div class="sidebar_wrapper"></div><!-- sidebar_wrapper -->
+<div class="sidebar_wrapper">
+	
+	<div class="sidebar_inner">
+		
+		<div class="sidebar_title_wrapper">
+			
+			<span class="sidebar_title">Types of Aviation Accidents</span>
+			
+			<img class="svg" src="<?php bloginfo('template_directory');?>/images/svgs/air3.svg"/>
+			
+		</div><!-- sidebar_title_wrapper -->
+		
+		
+		<ul>
+			<li><a href="">Airline Accidents</a></li>
+			<li><a href="">Airline Accidents</a></li>
+			<li><a href="">Airline Accidents</a></li>
+			<li><a href="">Airline Accidents</a></li>
+			<li><a href="">Airline Accidents</a></li>
+			<li><a href="">Airline Accidents</a></li>
+		</ul>
+		
+		
+	</div><!-- sidebar_inner -->
+	
+</div><!-- sidebar_wrapper -->
