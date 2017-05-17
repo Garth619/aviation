81dc2eca3bde2801634abbcbbec687d95d96e9f2
Garrett Cullen <garrettcullen@Garretts-iMac.local>
2017-05-17 14:54:46 -0700

    Push

---
+++ wp-content/themes/psb/page-attorney.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Template Name: Attorney Profile
+ *
+
+ */
+
+get_header(); ?>
+
+		
+
+			<?php include('banner.php');?>
+			
+			<div class="inner_main">
+				
+				
+				<div class="container_wrapper">
+				
+					<div class="container">
+					
+						<h1 class="title_header">Brian Panish</h1><!-- title_header -->
+						
+						<div class="purple_line_wrapper">
+							
+							<div class="purple_line"></div><!-- purple_line -->
+							
+							<div class="purple_square"></div><!-- purple_square -->
+							
+						</div><!-- purple_line_wrapper -->
+						
+						<div class="content">
+							
+							<?php // get_template_part( 'loop', 'page' ); ?>
+							
+							<h2>One of the country’s leading trial attorneys, Los Angeles personal injury lawyer Brian Panish has obtained some of the most significant jury verdicts in United States history on behalf of plaintiffs</h2>
+							
+							<p>His courtroom victories include a $4.9 billion record verdict in the landmark products liability case Anderson v. General Motors, and over thirty verdicts and settlements in excess of $10 million in personal injury, car accident, wrongful death and business litigation cases. He also has obtained more than 250 verdicts and settlements over $1 million dollars. Mr. Panish has obtained three jury verdicts in excess of $50 million in personal injury cases – more than any other attorney in California.  No other attorney in California has more 8-figure verdicts than Brian Panish.</p>
+
+							<p>Since 2011 alone, Brian Panish has obtained more than $250 million in verdicts, including, among others, a $36.5 million jury verdict against Schneider National for a big-rig crash that left a woman with severe spinal cord injuries, a $34.5 million verdict for a man who suffered a below the knee amputation after being hit by a tractor trailer, a $17.4 million verdict for a Ph.D. student who suffered brain and jaw injuries during a rear end chain collision accident, a $17 million jury verdict against the Los Angeles MTA in the wrongful death of a visually impaired passenger, a $17.845 million award in a wrongful death case involving four family members killed when a Marine jet crashed into their home, a $20 million jury verdict for a woman who suffered a brain injury in a hit-and-run accident involving a J.B. Hunt tractor trailer, a $17 million jury verdict for a 84-year old man hit by a bus resulting in a leg amputation, a $15.3 million jury verdict for a woman and her daughter after the woman was severely injured on a negligently driven Alameda Contra-Costa Transit District bus, a $10.45 million jury verdict for the family of a security guard killed on the CBS studio lot and a $8.3 million jury verdict in the first DePuy ASR Hip Implant case to go to trial.</p>
+							
+							<ul>
+								<li>National Law Journal’s Elite Trial Lawyers Hall of Fame – 2015</li>
+								<li>2016 Best Lawyers® Lawyer of the Year – Plaintiff’s Personal Injury Litigation – Los Angeles</li>
+								<li>2015 Best Lawyers® Lawyer of the Year – Product Liability Litigation – Los Angeles</li>
+							</ul>
+
+							<h3>VERDICTS AND SETTLEMENTS</h3>
+							
+							<ul>
+								<li>$4.9 Billion – Anderson v. General Motors Corp.</li>
+								<li>$3.3 Billion – LA County v. Tobacco Industry</li>
+								<li>$58 Million – Griggs v. Caterpillar, et al.</li>		
+							</ul>					
+							
+							
+						</div><!-- content -->
+					
+						
+					
+					</div><!-- container -->
+				
+				
+				
+				<?php include('sidebar-attorney.php');?>
+				
+				</div><!-- content_wrapper -->
+				
+				
+			</div><!-- inner_main -->
+			
+			
+			<?php get_footer(); ?>
---
+++ wp-content/themes/psb/sidebar-attorney.php
@@ -0,0 +1,34 @@
+<div class="sidebar_wrapper">
+	
+	<div class="sidebar_inner">
+		
+		
+		<img class="profile_pic" src="<?php bloginfo('template_directory');?>/images/attorney-panish.png"/>
+		
+		<a class="" href="">
+		
+		<div class="video_wrapper">
+			
+			<div class="video_title">Partner/Founder</div><!-- video_title -->
+			
+			<div class="play_button">
+				
+				
+					
+					<span class="tri"></span>
+					
+				
+				
+			</div><!-- play_button -->
+			
+		</div><!-- video_wrapper -->
+		
+		</a>
+	
+	</div><!-- sidebar_inner -->
+	
+	
+	
+	<div style="clear:both"></div>
+	
+</div><!-- sidebar_wrapper -->
