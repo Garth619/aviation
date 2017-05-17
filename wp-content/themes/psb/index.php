ae346425e00a93ec1d70f7dd7f4a9a33275624df
Garrett Cullen <garrettcullen@Garretts-iMac.local>
2017-05-17 10:36:53 -0700

    Push

--- wp-content/themes/psb/index.php
+++ wp-content/themes/psb/index.php
@@ -15,19 +15,117 @@
 
 get_header(); ?>
 
-		<div id="container">
-			<div id="content" role="main">
 
-			<?php
-			/*
-			 * Run the loop to output the posts.
-			 * If you want to overload this in a child theme then include a file
-			 * called loop-index.php and that will be used instead.
-			 */
-			get_template_part( 'loop', 'index' );
-			?>
-			</div><!-- #content -->
-		</div><!-- #container -->
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+		<?php include('banner.php');?>
+			
+			<div class="inner_main">
+				
+				
+				<div class="container_wrapper">
+				
+					<div class="container">
+					
+						<h1 class="title_header">Latest News/Blog</h1><!-- title_header -->
+						
+						<div class="purple_line_wrapper">
+							
+							<div class="purple_line"></div><!-- purple_line -->
+							
+							<div class="purple_square"></div><!-- purple_square -->
+							
+						</div><!-- purple_line_wrapper -->
+						
+						<div class="content">
+							
+							<div class="news_wrapper">
+								
+								<div class="mypost">
+									
+									<h2 class="myposts_title">Blog Post</h2><!-- myposts_title -->
+									
+									<span class="mypost_content">
+									
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+									
+									</span><!-- mypost_content -->
+									
+									<a class="my_read_more" href="">Read More</a><!-- read_more -->
+									
+								</div><!-- mypost -->
+								
+								<div class="mypost">
+									
+									<h2 class="myposts_title">Blog Post</h2><!-- myposts_title -->
+									
+									<span class="mypost_content">
+									
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+									
+									</span><!-- mypost_content -->
+									
+									<a class="my_read_more" href="">Read More</a><!-- read_more -->
+									
+								</div><!-- mypost -->
+								
+								<div class="mypost">
+									
+									<h2 class="myposts_title">Blog Post</h2><!-- myposts_title -->
+									
+									<span class="mypost_content">
+									
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+									
+									</span><!-- mypost_content -->
+									
+									<a class="my_read_more" href="">Read More</a><!-- read_more -->
+									
+								</div><!-- mypost -->
+								
+								<div class="mypost">
+									
+									<h2 class="myposts_title">Blog Post</h2><!-- myposts_title -->
+									
+									<span class="mypost_content">
+									
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+									
+									</span><!-- mypost_content -->
+									
+									<a class="my_read_more" href="">Read More</a><!-- read_more -->
+									
+								</div><!-- mypost -->
+								
+								<div class="mypost">
+									
+									<h2 class="myposts_title">Blog Post</h2><!-- myposts_title -->
+									
+									<span class="mypost_content">
+									
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+									
+									</span><!-- mypost_content -->
+									
+									<a class="my_read_more" href="">Read More</a><!-- read_more -->
+									
+								</div><!-- mypost -->
+								
+							</div><!-- news_wrapper -->
+							
+						</div><!-- content -->
+					
+						
+					
+					</div><!-- container -->
+				
+				
+				
+				<?php get_sidebar();?>
+				
+				</div><!-- container_wrapper -->
+				
+				
+			</div><!-- inner_main -->
+			
+			
+			<?php get_footer(); ?>
--- wp-content/themes/psb/page.php
+++ wp-content/themes/psb/page.php
@@ -30,6 +30,8 @@ get_header(); ?>
 						
 						<div class="content">
 							
+							<?php // get_template_part( 'loop', 'page' ); ?>
+							
 							<h2>Air ambulances have been patrolling the skies since World War I. Designed to evacuate injured people in remote or inaccessible locations, these aircraft have no doubt helped save the lives of thousands.</h2>
 							
 							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore <a href="">eu fugiat nulla pariatur</a>. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p> 
@@ -46,7 +48,7 @@ get_header(); ?>
 							
 						</div><!-- content -->
 					
-						<?php // get_template_part( 'loop', 'page' ); ?>
+						
 					
 					</div><!-- container -->
 				
