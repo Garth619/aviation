7249c754fece481d8139d6f9f9cbfaffb52befc1
Garrett Cullen <garrettcullen@Garretts-iMac.local>
2017-05-18 10:56:28 -0700

    Push

---
+++ wp-content/themes/psb/page-contact.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Template Name: Contact
+ *
+
+ */
+
+get_header(); ?>
+
+		
+
+			<?php include('banner.php');?>
+			
+			<div class="inner_main">
+				
+				
+				<div class="container_wrapper">
+				
+					<div class="container nosidebar">
+					
+						<h1 class="title_header"><?php the_title();?></h1><!-- title_header -->
+						
+						<div class="purple_line_wrapper">
+							
+							<div class="purple_line"></div><!-- purple_line -->
+							
+							<div class="purple_square"></div><!-- purple_square -->
+							
+						</div><!-- purple_line_wrapper -->
+						
+					
+							
+							<div class="contact_wrapper">
+								
+								<span class="contact_info">Phone us toll-free at <a class="contact_tel" href="tel:18778001700">1-877-800-1700</a> to set up a free consultation now.</span><!-- contact_info -->
+								
+								<div class="contact_location_wrapper">
+									
+									<div class="contact_single_location">
+										
+										
+										<div class="location_left">
+											
+											<span class="contact_address">1111 Santa Monica Blvd.<br/>
+											#700<br/>
+											Los Angeles, California<br/>
+											90025</span>
+											
+											<a class="contact_get_directions" href="">
+												
+												<div class="contact_gradient">
+													
+													<div class="contact_inner_gradient">
+														
+														<span>Get Directions</span>
+														
+													</div><!-- contact_inner_gradient -->
+													
+												</div><!-- contact_gradient -->
+												
+											</a><!-- contact_get_directions -->
+											
+											<div class="black_line_wrapper">
+												
+												<div class="black_line"></div><!-- black_line -->
+												
+												<div class="black_square"></div><!-- black_square -->
+												
+											</div><!-- black_line_wrapper -->
+											
+										</div><!-- location_left -->
+										
+										<div class="location_map">
+											
+											
+											
+										</div><!-- location_map -->
+										
+										
+									</div><!-- contact_single_location -->
+									
+								</div><!-- contact_location_wrapper -->
+								
+							</div><!-- contact_wrapper -->
+							
+						
+					
+						
+					
+					</div><!-- container -->
+				
+				
+				
+			
+				
+				</div><!-- content_wrapper -->
+				
+				
+			</div><!-- inner_main -->
+			
+			
+			<?php get_footer(); ?>
---
+++ wp-content/themes/psb/page-padirectory.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Template Name: PA Directory
+ *
+
+ */
+
+get_header(); ?>
+
+		
+
+			<?php include('banner.php');?>
+			
+			<div class="inner_main">
+				
+				
+				<div class="container_wrapper">
+				
+					<div class="container nosidebar">
+					
+						<h1 class="title_header"><?php the_title();?></h1><!-- title_header -->
+						
+						<div class="purple_line_wrapper">
+							
+							<div class="purple_line"></div><!-- purple_line -->
+							
+							<div class="purple_square"></div><!-- purple_square -->
+							
+						</div><!-- purple_line_wrapper -->
+						
+					
+							
+							<div class="pa_wrapper">
+								
+								<ul>
+									<li><a href="">Airline Accidents</a></li>
+									<li><a href="">Airline Accidents</a></li>
+									<li><a href="">Airline Accidents</a></li>
+									<li><a href="">Airline Accidents</a></li>
+									<li><a href="">Airline Accidents</a></li>
+									<li><a href="">Airline Accidents</a></li>
+								</ul>
+								
+							</div><!-- pa_wrapper -->
+							
+						
+					
+						
+					
+					</div><!-- container -->
+				
+				
+				
+			
+				
+				</div><!-- content_wrapper -->
+				
+				
+			</div><!-- inner_main -->
+			
+			
+			<?php get_footer(); ?>
