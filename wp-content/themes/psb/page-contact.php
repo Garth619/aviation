e0c9802ad26d06aa0f5711c36c733fc541c3cd53
Garrett Cullen <garrettcullen@Garretts-iMac.local>
2017-05-18 14:46:27 -0700

    Push

--- wp-content/themes/psb/page-contact.php
+++ wp-content/themes/psb/page-contact.php
@@ -52,7 +52,7 @@ get_header(); ?>
 													
 													<div class="contact_inner_gradient">
 														
-														<span>Get Directions</span>
+														<span>Directions</span>
 														
 													</div><!-- contact_inner_gradient -->
 													
@@ -70,9 +70,73 @@ get_header(); ?>
 											
 										</div><!-- location_left -->
 										
-										<div class="location_map">
+										<div class="location_map_wrapper">
+											
+											<div class="inner_map">
 											
 											
+											<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3306.8518965586186!2d-118.49048808478571!3d34.02201238061405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2a4cc9df1a7f5%3A0xea3a529c70d50778!2s1111+Santa+Monica+Blvd+%23700%2C+Santa+Monica%2C+CA+90401!5e0!3m2!1sen!2sus!4v1495141832630" frameborder="0" style="border:0" allowfullscreen></iframe>
+											
+											</div><!-- inner_map -->
+											
+										</div><!-- location_map -->
+										
+										
+									</div><!-- contact_single_location -->
+									
+									
+									
+									<div id="tablet_black_line" class="black_line_wrapper">
+												
+												<div class="black_line"></div><!-- black_line -->
+												
+												<div class="black_square"></div><!-- black_square -->
+												
+											</div><!-- black_line_wrapper -->
+									
+									
+									<div class="contact_single_location">
+										
+										
+										<div class="location_left">
+											
+											<span class="contact_address">17500 Red Hill Ave.<br/> 
+											#100<br/>
+											Irvine, California<br/>
+											92614</span>
+											
+											<a class="contact_get_directions" href="">
+												
+												<div class="contact_gradient">
+													
+													<div class="contact_inner_gradient">
+														
+														<span>Directions</span>
+														
+													</div><!-- contact_inner_gradient -->
+													
+												</div><!-- contact_gradient -->
+												
+											</a><!-- contact_get_directions -->
+											
+											<div class="black_line_wrapper">
+												
+												<div class="black_line"></div><!-- black_line -->
+												
+												<div class="black_square"></div><!-- black_square -->
+												
+											</div><!-- black_line_wrapper -->
+											
+										</div><!-- location_left -->
+										
+										<div class="location_map_wrapper">
+											
+											<div class="inner_map">
+											
+											
+											<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3319.56791703904!2d-117.85573428479435!3d33.69425068070484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dcdebf7c958f7d%3A0x19937cc64985a202!2s17500+Red+Hill+Ave+%23100%2C+Irvine%2C+CA+92614!5e0!3m2!1sen!2sus!4v1495143889315" frameborder="0" style="border:0" allowfullscreen></iframe>
+											
+											</div><!-- inner_map -->
 											
 										</div><!-- location_map -->
 										
