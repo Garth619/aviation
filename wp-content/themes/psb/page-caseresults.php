5c8748ae50c0427a7052db28d66b2ef36b15dcf2
Garrett Cullen <garrettcullen@Garretts-iMac.local>
2017-05-18 15:11:37 -0700

    Push

--- wp-content/themes/psb/page-caseresults.php
+++ wp-content/themes/psb/page-caseresults.php
@@ -16,9 +16,9 @@ get_header(); ?>
 				
 				<div class="container_wrapper">
 				
-					<div class="container">
+					<div class="container nosidebar">
 					
-						<h1 class="title_header">nationwide aviation disaster attorneys</h1><!-- title_header -->
+						<h1 class="title_header"><?php the_title();?></h1><!-- title_header -->
 						
 						<div class="purple_line_wrapper">
 							
@@ -28,25 +28,26 @@ get_header(); ?>
 							
 						</div><!-- purple_line_wrapper -->
 						
-						<div class="content">
+					
 							
-							<?php // get_template_part( 'loop', 'page' ); ?>
 							
-							<h2>Air ambulances have been patrolling the skies since World War I. Designed to evacuate injured people in remote or inaccessible locations, these aircraft have no doubt helped save the lives of thousands.</h2>
-							
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore <a href="">eu fugiat nulla pariatur</a>. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p> 
-
-							<h3>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit</h3>
-							
-							<ul>
-							<li>Sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt</li> 
-							<li>Neque porro quisquam est, qui dolorem ipsum quia dolor sit ametunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium mollit anim idest laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</li>
-							<ul>
+							<div class="success_intro">
+								
+								
+								<p>The decision to hire an attorney after a devastating aviation accident is not one to be taken lightly. Panish Shea & Boyle LLP encourages those seeking the assistance of any aviation lawyer to ask about the aviation case results they've achieved and the aviation cases they've tried to verdict. A qualified plane accident law firm will have substantial results in the type of accident that you or your loved one have suffered through. Below are examples of Panish Shea & Boyle LLP's successes in the arena of Aviation Law.</p>
+								
+								
+							</div><!-- success_intro -->
 							
 							
 							
 							
-						</div><!-- content -->
+							<div class="featured_case">
+								
+								
+								
+							</div><!-- featured_case -->
+						
 					
 						
 					
@@ -54,7 +55,7 @@ get_header(); ?>
 				
 				
 				
-				<?php get_sidebar();?>
+			
 				
 				</div><!-- content_wrapper -->
 				
