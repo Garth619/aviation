548a05a31ca6422b534b914f4bd250c612c4c36e
Garrett Cullen <garrettcullen@Garretts-iMac.local>
2017-05-19 10:20:14 -0700

    Push

--- wp-content/themes/psb/page-caseresults.php
+++ wp-content/themes/psb/page-caseresults.php
@@ -57,7 +57,12 @@ get_header(); ?>
 									
 									<div class="success_content">
 										
+
+										
 										<p>Yoon v. United States -- Panish Shea & Boyle LLP represented Don Yoon against the United States government and Boeing for negligence after Yoons wife, two daughters and mother-in-law were tragically killed when a Navy/Marine Corps jet’s fuel tanks failed, causing it to crash into the Yoon San Diego, California home in 2008. Previously released recordings of conversations between federal air controllers and the pilot in question, who was in training and survived by parachute injection prior to the crash, show he was repeatedly offered a chance to land the plane at the a near by San Diego Navy base away from civilians avoiding the horror. The United States Government admitted liability for the crash and the family were awarded $17, 845,000 on December 28, 2011. The firm represented the mother-in-law’s husband, children and mother in their wrongful death claims - the highest wrongful death sentence against the United States and 20th highest verdict.</p>
+										
+										
+								
 										
 									</div><!-- success_content -->
 									
@@ -65,7 +70,139 @@ get_header(); ?>
 								
 							</div><!-- featured_case -->
 						
-					
+							
+							<div class="bottom_cases_wrapper">
+								
+								<div class="left_case_col case_col">
+									
+									
+									<div class="case_cat_wrapper">
+									
+									<span class="large_case_title">commercial  airplane crashes</span><!-- large_case_title -->
+									
+									<div class="lower_black_line_wrapper">
+										
+										<div class="lower_black_line"></div><!-- lower_black_line -->
+										
+										<div class="lower_black_square"></div><!-- lower_black_square -->
+										
+									</div><!-- lower_black_line_wrapper -->
+									
+									
+									<div class="lower_single_case">
+										
+										<span class="lower_case_title">$15,750,000</span><!-- lower_case_title -->
+										<span class="lower_case_subtitle">Wrongful Death Airplane Crash Settlement</span><!-- lower_case_subtitle -->
+										
+										<div class="single_case_content">
+											
+											<p>Doe vs. Commercial Airline - Confidential settlement for the family of a wrongful death victim in a highly publicized commercial aviation crash.</p>
+											
+											
+										</div><!-- single_case_content -->
+										
+									</div><!-- lower_single_case -->
+									
+									<div class="lower_single_case">
+										
+										<span class="lower_case_title">$15,000,000</span><!-- lower_case_title -->
+										<span class="lower_case_subtitle">Airplane Accident Wrongful Death Verdict</span><!-- lower_case_subtitle -->
+										
+										<div class="single_case_content">
+											
+											<p>Wu v. Singapore Airlines - Wrongful Death verdict in crash of Singapore Airlines Boeing 747-400 which attempted to take off on a closed runway during Typhoon Xangsane, the plane crashed and the fuselage broke into three sections, killing 82 people and injuring 169 others. Only one case from the crash was tried to jury verdict, and that family was represented by Brian Panish and Kevin Boyle.</p>
+											
+											
+										</div><!-- single_case_content -->
+										
+									</div><!-- lower_single_case -->
+									
+
+									
+								</div><!-- case_cat_wrapper -->
+									
+									
+									
+									<div class="case_cat_wrapper">
+									
+									<span class="large_case_title">Helicopter Crash</span><!-- large_case_title -->
+									
+									<div class="lower_black_line_wrapper">
+										
+										<div class="lower_black_line"></div><!-- lower_black_line -->
+										
+										<div class="lower_black_square"></div><!-- lower_black_square -->
+										
+									</div><!-- lower_black_line_wrapper -->
+									
+									
+									<div class="lower_single_case">
+										
+										<span class="lower_case_title">$13,550,000</span><!-- lower_case_title -->
+										<span class="lower_case_subtitle">helicopter crash Verdict</span><!-- lower_case_subtitle -->
+										
+										<div class="single_case_content">
+											
+											<p>Beltran/Carns v. Boeing - Largest known settlement for military personnel injured during the war in Iraq awarded to Juan Beltran and Ron Carns for injuries they received. Beltran was represented by Kevin Boyle and was awarded $11,247,500 for injuries he suffered in a helicopter crash in Iraq which left him paralyzed.</p>
+											
+											
+										</div><!-- single_case_content -->
+										
+									</div><!-- lower_single_case -->
+									
+									<div class="lower_single_case">
+										
+										<span class="lower_case_title">$7,300,000</span><!-- lower_case_title -->
+										<span class="lower_case_subtitle">helicopter wrongful death verdict</span><!-- lower_case_subtitle -->
+										
+										<div class="single_case_content">
+											
+											<p>Doe vs. Helicopter Tour Operator - Wrongful death caused by the crash of a sightseeing tour helicopter.</p>
+											
+											
+										</div><!-- single_case_content -->
+										
+									</div><!-- lower_single_case -->
+									
+									<div class="lower_single_case">
+										
+										<span class="lower_case_title">$7,300,000</span><!-- lower_case_title -->
+										<span class="lower_case_subtitle">helicopter wrongful death verdict</span><!-- lower_case_subtitle -->
+										
+										<div class="single_case_content">
+											
+											<p>Doe vs. Helicopter Tour Operator - Wrongful death caused by the crash of a sightseeing tour helicopter.</p>
+											
+											
+										</div><!-- single_case_content -->
+										
+									</div><!-- lower_single_case -->
+									
+									<div class="lower_single_case">
+										
+										<span class="lower_case_title">$7,300,000</span><!-- lower_case_title -->
+										<span class="lower_case_subtitle">helicopter wrongful death verdict</span><!-- lower_case_subtitle -->
+										
+										<div class="single_case_content">
+											
+											<p>Doe vs. Helicopter Tour Operator - Wrongful death caused by the crash of a sightseeing tour helicopter.</p>
+											
+											
+										</div><!-- single_case_content -->
+										
+									</div><!-- lower_single_case -->
+									
+									
+									
+									
+									</div><!-- case_cat_wrapper -->
+									
+									
+								</div><!-- left_case_col -->
+								
+								<div class="right_case_col case_col"></div><!-- right_case_col -->
+								
+							</div><!-- bottom_cases_wrapper -->
 						
 					
 					</div><!-- container -->
