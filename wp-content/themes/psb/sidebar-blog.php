44d7d998424632b0a0ce77f576e05c2586cf92ae
Garrett Cullen <garrettcullen@Garretts-iMac.local>
2017-05-25 10:24:04 -0700

    Push

--- wp-content/themes/psb/sidebar-blog.php
+++ wp-content/themes/psb/sidebar-blog.php
@@ -6,7 +6,24 @@
 			
 			<span class="sidebar_title">Recent News</span>
 			
-			<img class="svg" src="<?php bloginfo('template_directory');?>/images/svgs/air3.svg"/>
+			<svg id="air_sidebar" viewBox="0 -5 200.72 117.06">
+			
+			
+				<defs>
+					
+					<style>.cls-1{fill:none;stroke:#193cc9;stroke-linecap:round;stroke-linejoin:round;stroke-width:4px;}</style>
+			
+				</defs>
+				
+				<line class="cls-1" x1="1" y1="37.29" x2="28.2" y2="37.29"/>
+				<path class="cls-1" d="M42.3,37.3h87.4c11.6,0,20.8-10.4,17.4-23.6C142-1.1,124.4-2.8,116,7.8" transform="translate(0 -0.01)"/>
+				<path class="cls-1" d="M91.2,55.3h87.4c11.7,0,20.8-10.4,17.4-23.6-5.2-14.8-22.8-16.5-31.2-5.9" transform="translate(0 -0.01)"/>
+				<path class="cls-1" d="M1.2,73.2H89.1c11.2,0,20.6,10.3,17.3,23.4-5.2,14.8-22.8,15.9-31,6.1" transform="translate(0 -0.01)"/>
+				<line class="cls-1" x1="1" y1="55.29" x2="8.3" y2="55.29"/>
+				<line class="cls-1" x1="22.2" y1="55.29" x2="75.3" y2="55.29"/>
+				
+			
+			</svg>
 			
 		</div><!-- sidebar_title_wrapper -->
 		
@@ -24,7 +41,24 @@
 			
 			<span class="sidebar_title">Categories</span>
 			
-			<img class="svg" src="<?php bloginfo('template_directory');?>/images/svgs/air3.svg"/>
+			<svg id="air_sidebar" viewBox="0 -5 200.72 117.06">
+			
+			
+				<defs>
+					
+					<style>.cls-1{fill:none;stroke:#193cc9;stroke-linecap:round;stroke-linejoin:round;stroke-width:4px;}</style>
+			
+				</defs>
+				
+				<line class="cls-1" x1="1" y1="37.29" x2="28.2" y2="37.29"/>
+				<path class="cls-1" d="M42.3,37.3h87.4c11.6,0,20.8-10.4,17.4-23.6C142-1.1,124.4-2.8,116,7.8" transform="translate(0 -0.01)"/>
+				<path class="cls-1" d="M91.2,55.3h87.4c11.7,0,20.8-10.4,17.4-23.6-5.2-14.8-22.8-16.5-31.2-5.9" transform="translate(0 -0.01)"/>
+				<path class="cls-1" d="M1.2,73.2H89.1c11.2,0,20.6,10.3,17.3,23.4-5.2,14.8-22.8,15.9-31,6.1" transform="translate(0 -0.01)"/>
+				<line class="cls-1" x1="1" y1="55.29" x2="8.3" y2="55.29"/>
+				<line class="cls-1" x1="22.2" y1="55.29" x2="75.3" y2="55.29"/>
+				
+			
+			</svg>
 			
 		</div><!-- sidebar_title_wrapper -->
 		
